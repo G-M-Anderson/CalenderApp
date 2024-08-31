358f93d50a133c721365aa42bf702c14e7112bfd
Philoso-Raptor <gande12@cs.okstate.edu>
2024-08-31 12:01:23 -0500

    Here is a basic layout for the project to build on later!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CalenderApp
 Calendar App for Software Engineering Fall 2024
+
+Made some basic folders Calendar, Config and Tests to get us started.
+
---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+
+# Defining main function
+def main():
+
+    print ("Calendar App for Fall Software Engineering 2024")
+    # Created a basic main file with placeholder print out -Garrett.
+    
+if __name__=="__main__":
+    main()
+
