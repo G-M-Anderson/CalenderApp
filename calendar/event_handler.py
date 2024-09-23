37b63221640d590477e262e2b48903d3f571ab3d
Elaina-H <139798999+Elaina-H@users.noreply.github.com>
2024-09-23 10:06:08 -0500

    Update event_handler.py

--- calendar/event_handler.py
+++ calendar/event_handler.py
@@ -1,1 +1,19 @@
 #file to contain functions for creating events
+
+import pygame
+pygame.init()
+#GUI here, open window
+
+#variable like 
+program_running = TRUE
+#begin event handling loop
+while not (program_running):
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      #exit behavior
+
+    #handle all possible event types
+
+pygame.quit()
+quit()
+
