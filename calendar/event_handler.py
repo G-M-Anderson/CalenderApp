dc514dadecb230329c854865e2a57c64b05b2b6c
Elaina-H <139798999+Elaina-H@users.noreply.github.com>
2024-09-23 10:08:48 -0500

    Update event_handler.py

--- calendar/event_handler.py
+++ calendar/event_handler.py
@@ -5,13 +5,13 @@ pygame.init()
 #GUI here, open window
 
 #variable like 
-program_running = TRUE
+program_running = True
 #begin event handling loop
-while not (program_running):
+while (program_running):
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       #exit behavior
-
+      program_running = False
     #handle all possible event types
 
 pygame.quit()
